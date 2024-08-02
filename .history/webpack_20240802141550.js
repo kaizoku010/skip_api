efa0d0710af664cbe6845af5610f78162346da0f
mRodneyMo <motokads@gmail.com>
2024-08-02 14:16:07 +0300

    installs

---
+++ .history/webpack_20240802141550.js
@@ -0,0 +1,20 @@
+// webpack.js
+const path = require('path');
+
+module.exports = {
+  entry: './server.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+  
+};
